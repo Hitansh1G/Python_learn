94023e7d2836ee5bbc17cb73d5f6e2d81151e382
Hitansh1G <86662038+Hitansh1G@users.noreply.github.com>
2021-09-18 19:49:47 +0530

    variables

---
+++ variables.py
@@ -0,0 +1,12 @@
+age = 20
+age = age + 20
+# we dont need to give any datatype to the variables 
+# we can just write any variable and its value 
+price = 19.85
+first_name = "Hitansh "
+answer = False
+# python is a k sensitive language 
+print(age)
+print(price)
+print(first_name)
+print(answer)
