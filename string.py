e2d014b65a95cadced569ab29f8e640fbacc8fea
Hitansh1G <86662038+Hitansh1G@users.noreply.github.com>
2021-09-25 13:28:33 +0530

    string

---
+++ string.py
@@ -0,0 +1,6 @@
+course = " Hitansh is great"
+print(course.removeprefix)
+print(course.capitalize)
+print(course.center)
+#there are many more types of inbuilt functions in python 
+# they are writen when we write course. 
