10be0386c0f9cfb3b227dc03dbf53b5d5df9d6e1
Hitansh1G <86662038+Hitansh1G@users.noreply.github.com>
2021-09-25 13:12:47 +0530

    type conversion

---
+++ typeConcersion.py
@@ -0,0 +1,15 @@
+birth_year = input( " Enter your birth year: ")
+# age = 2020 - birth_year
+# this will show you an error coz we are inputing a string value
+# and subtracting it from an int value
+age = 2020 - int (birth_year)
+print ( age )
+
+int(birth_year)  
+# by this way we type caste the string value into an int value 
+
+similarly others are 
+int()
+float()
+bool()
+str()
