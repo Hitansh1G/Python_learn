aae4d27c2686b5aab35b874a90b630a597b9700f
Hitansh1G <86662038+Hitansh1G@users.noreply.github.com>
2021-09-27 05:00:37 +0530

    loops

--- loop.py
+++ loop.py
@@ -8,10 +8,12 @@ while i < 10 :
 
 list = [1,2,3,4,5,6]
 for items in numbers:
-    print list
+    print (list)
 
 
 i = 0
 while i < len(list):
     print (i)
     i = i + 1
+
+# 2 ways to use loops
