300713d86d451cc1356d28d2db44fd88b09b26af
Hitansh1G <86662038+Hitansh1G@users.noreply.github.com>
2021-09-26 20:57:00 +0530

    loops

--- loop.py
+++ loop.py
@@ -4,3 +4,14 @@ while i < 10 :
     print(i)
     print ( i * '*')
     i = i + 1
+
+
+list = [1,2,3,4,5,6]
+for items in numbers:
+    print list
+
+
+i = 0
+while i < len(list):
+    print (i)
+    i = i + 1
