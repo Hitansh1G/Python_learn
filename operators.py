ccaa8cdc07564115ff338a3cf13c5ab8d40cfa11
Hitansh1G <86662038+Hitansh1G@users.noreply.github.com>
2021-09-26 15:32:31 +0530

    operations

--- operators.py
+++ operators.py
@@ -12,3 +12,14 @@ print (10 % 3)
 # 30
 # 1000
 # 1
+x = 3 > 2
+print (x)
+# this wil return a bool value
+price = 25 
+print ( price > 10 and price < 40)
+print ( not price > 10 and price < 40)
+
+# similarly we have more inbuilt operators like "not"
+# and(both)
+# or ( atleast one)
+# not()
