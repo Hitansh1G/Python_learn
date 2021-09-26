08f433839512fbcb3ac9f1f74e0618d2da233465
Hitansh1G <86662038+Hitansh1G@users.noreply.github.com>
2021-09-26 20:43:13 +0530

    list

---
+++ list.py
@@ -0,0 +1,14 @@
+names = ["hitansh" , "john" , " mosh", "sam", "aryan"]
+print(names)
+print(names[0])
+print(names[-1])
+print(names[-2])
+print(names[1])
+print(names[2])
+print(names[0:3])
+# for normal integer values in any array ➡️ type name. 
+# and saare inbuilt functions show ho jayenge
+
+
+
+
